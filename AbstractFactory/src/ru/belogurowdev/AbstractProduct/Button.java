2887c6b28f1f7ab0adfd4bc4c75fab0510495ec1
Alex Belogurow <alexbelogur10@gmail.com>
2017-08-03 15:25:15 +0300

    test

--- AbstractFactory/src/ru/belogurowdev/AbstractProduct/Button.java
+++
@@ -1,5 +0,0 @@
-package ru.belogurowdev.AbstractProduct;
-
-public interface Button {
-    void onClick();
-}
--- AbstractFactory/src/ru/belogurowdev/AbstractProduct/Window.java
+++
@@ -1,6 +0,0 @@
-package ru.belogurowdev.AbstractProduct;
-
-public interface Window {
-    void setBorderColor(String borderColor);
-    void setBackgroundColor(String backgroundColor);
-}
